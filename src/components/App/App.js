c22cbb8cc01cbf56550097e3411289160273a43f
Aleksandr Shelist <a.shelist@corp.mail.ru>
2021-12-05 16:50:17 +0300

    fix typo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,7 @@ import { IosPage } from '../../pages/IosPage';
 import { AndroidPage } from '../../pages/AndroidPage';
 
 function App() {
-  if (isIOS) {
+  if (isIOS()) {
     return <IosPage />
   }
 
